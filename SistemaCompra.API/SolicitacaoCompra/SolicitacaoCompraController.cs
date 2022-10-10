46cdd3f4034f210f011eab5c9329ee6c59781bf4
@claudenes <claudenes@gmail.com>
2022-10-10 12:28:59 -0300

    Primeiro Commit

---
+++ SistemaCompra.API/SolicitacaoCompra/SolicitacaoCompraController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SistemaCompra.Application.SolicitacaoCompra.Command.RegistrarCompra;
+
+namespace SistemaCompra.API.SolicitacaoCompra
+{
+    public class SolicitacaoCompraController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public SolicitacaoCompraController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        [HttpPost, Route("solicitacaocompra/salvar")]
+        public IActionResult SolicitacaoCompra([FromBody] RegistrarCompraCommand registrarcompraCommand)
+        {
+            _mediator.Send(registrarcompraCommand);
+            return StatusCode(201);
+        }
+    }
+}
